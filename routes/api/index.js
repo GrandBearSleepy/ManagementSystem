75473a92483ca01d9c8b3513afe9029a4236e756
Leon <hq_li1982@hotmail.com>
2021-02-06 20:14:56 +0800

    fix problem

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const customerRoutes = require('./customers');
 const cleanerRouters = require('./cleaners');
-const userRouters= require('./user')
+
 
 
 router.use('/customers', customerRoutes);
--- routes/api/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userController = require('../../controllers/usersController');
-
-
-router
-  .route('/users')
-  .get(userController.login)
-  .put(userController.register)
