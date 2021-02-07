4c99926283eb3113f2f55ffa72968ae4e6c73bdc
Leon <hq_li1982@hotmail.com>
2021-02-07 10:34:34 +0800

    add some style

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Row,List } from 'antd';
 import './index.css';
 
+
 export default function Work() {
+
+  const data=['Test','Total:']
+
   return (
     <div className="site-card-wrapper">
       <Row gutter={16}>
         <Col span={8}>
-          <Card className="client" title="Clients" bordered={false}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque maiores soluta minus eius blanditiis mollitia labore id natus fuga nihil vel tempora eligendi ab, quia voluptas impedit nesciunt esse assumenda.
+          <Card
+   
+            className="client"
+            title="Clients"
+            bordered={false}>
+            <List
+              size="large"
+              header={<div>Header</div>}
+              footer={<div>Footer</div>}
+              bordered
+              dataSource={data}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
         </Card>
         </Col>
         <Col span={8}>
-          <Card className="cleaner" title="Cleaners" bordered={false}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti odit dolor cupiditate dicta at culpa repellendus ipsum quisquam officia. Animi iusto sed quisquam neque saepe reiciendis deleniti officia, voluptates repudiandae!
+          <Card
+            className="cleaner"
+            title="Cleaners"
+            bordered={false}>
+            <List
+              size="large"
+              header={<div>Header</div>}
+              footer={<div>Footer</div>}
+              bordered
+              dataSource={data}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
         </Card>
         </Col>
         <Col span={8}>
-          <Card className="job" title="Jobs" bordered={false}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae amet fugit inventore nesciunt tenetur illo suscipit blanditiis neque aliquam, rerum doloremque officiis quaerat, natus repudiandae quod earum odio adipisci animi.
+          <Card
+            className="job" title="Jobs" bordered={false}>
+            <List
+              size="large"
+              header={<div>Header</div>}
+              footer={<div>Footer</div>}
+              bordered
+              dataSource={data}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
         </Card>
         </Col>
       </Row>
